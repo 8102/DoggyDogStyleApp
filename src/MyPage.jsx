3ae9360c4290e99b636ef259098dfbaa4219f6e1
Yanice <yanice.rabaini@gmail.com>
2016-12-09 13:22:23 +0100

    Firebase clés

--- src/MyPage.jsx
+++ src/MyPage.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import firebase from 'firebase'
+
+var firebase = firebase.initializeApp({
+    apiKey: " AIzaSyCdoJ5Q10uHkmUHMOiI_l-q05CmraQ0yMc",
+authDomain: "lab3-462d5.firebaseio.com",
+databaseURL: "https://lab3-462d5.firebaseio.com/",
+storageBucket: "gs://lab3-462d5.appspot.com",
+})
 
 export default class MyPage extends React.Component {
 
